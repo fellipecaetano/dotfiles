4b4facced8dad577217e1a81e487a26933c0fd53
Fellipe <fellipe.caetano4@gmail.com>
2018-01-08 21:58:13 -0200

    Hard-code ahgora credentials

--- ahgora_punch.sh
+++ ahgora_punch.sh
@@ -1,3 +1,9 @@
+#!/bin/sh
+
+AHGORA_ACCOUNT="147"
+AHGORA_PASSWORD="0603"
+aHGORA_IDENTITY="ed798153c546fdc15a5176bec21e6573"
+
 curl -X POST \
   -F "account=$AHGORA_ACCOUNT" \
   -F "password=$AHGORA_PASSWORD" \
