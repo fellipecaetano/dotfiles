39d01b83efdf72aed1379507ac4cdc89933afa68
Fellipe <fellipe.caetano4@gmail.com>
2018-01-08 21:51:25 -0200

    Create script that punches your workcard

---
+++ ahgora_punch.sh
@@ -0,0 +1,5 @@
+curl -X POST \
+  -F "account=$AHGORA_ACCOUNT" \
+  -F "password=$AHGORA_PASSWORD" \
+  -F "identity=$AHGORA_IDENTITY" \
+  https://www.ahgora.com.br/batidaonline/verifyIdentification
